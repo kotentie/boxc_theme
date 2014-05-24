1164d01df8687e79d2645939e243af2f40134d9b
Jake <jaker@Jakes-MacBook.local>
2014-05-24 17:13:59 -0500

    add link lists to footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,21 @@
 <footer class="content-info" role="contentinfo">
-  <div class="container-fluid">
+  <div class="container-fluid footer-container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
-    <nav class="footer-navigation">
-    <?php wp_nav_menu( array( 'theme_location' => 'footer_navigation' ) ); ?>
-	</nav>
+  	<div class="row">
+	    <div class="footer-boxc col-md-2">
+	    <h5> Boxc </h5>
+	    <?php wp_nav_menu( array( 'theme_location' => 'footer_navigation_boxc' ) ); ?>
+		</div>
+		<div class="footer-boxc col-md-2">
+	    <h5> Social </h5>
+	    <?php wp_nav_menu( array( 'theme_location' => 'footer_navigation_social' ) ); ?>
+		</div>
+	    <div class="footer-boxc col-md-2">
+	    <h5> Contact Us </h5>
+	    <?php wp_nav_menu( array( 'theme_location' => 'footer_navigation_contact' ) ); ?>
+		</div>
+	</div>
     <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
   </div>
 </footer>
-
 <?php wp_footer(); ?>
