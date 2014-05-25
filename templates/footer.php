a44b8035c0b249dae9c18d14464f5b8264190929
Jake <jaker@Jakes-MacBook.local>
2014-05-24 19:12:49 -0500

    add styles to footer

--- templates/footer.php
+++ templates/footer.php
@@ -2,17 +2,19 @@
   <div class="container-fluid footer-container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   	<div class="row">
-	    <div class="footer-boxc col-md-2">
-	    <h5> Boxc </h5>
-	    <?php wp_nav_menu( array( 'theme_location' => 'footer_navigation_boxc' ) ); ?>
-		</div>
-		<div class="footer-boxc col-md-2">
-	    <h5> Social </h5>
-	    <?php wp_nav_menu( array( 'theme_location' => 'footer_navigation_social' ) ); ?>
-		</div>
-	    <div class="footer-boxc col-md-2">
-	    <h5> Contact Us </h5>
-	    <?php wp_nav_menu( array( 'theme_location' => 'footer_navigation_contact' ) ); ?>
+  		<div class="footer-link-container col-md-9">
+		    <div class="footer-links col-md-3">
+		    <h5> Boxc </h5>
+		    <?php wp_nav_menu( array( 'theme_location' => 'footer_navigation_boxc' ) ); ?>
+			</div>
+			<div class="footer-links col-md-3">
+		    <h5> Social </h5>
+		    <?php wp_nav_menu( array( 'theme_location' => 'footer_navigation_social' ) ); ?>
+			</div>
+		    <div class="footer-links col-md-3">
+		    <h5> Contact Us </h5>
+		    <?php wp_nav_menu( array( 'theme_location' => 'footer_navigation_contact' ) ); ?>
+			</div>
 		</div>
 	</div>
     <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
