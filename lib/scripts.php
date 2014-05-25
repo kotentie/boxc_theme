eb0652c3163fc694b71d6813500d1a478b9982bc
Jake <jaker@Jakes-MacBook.local>
2014-05-25 18:58:37 -0500

    add featured in section

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c641a3ec359af8f0f494a988b5bb1122');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e07beeec3c4b71879a21007c16db8440');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
