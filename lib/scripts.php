3b151bc3840bbf42a028ceb0e35cb3e0671bd6ff
Jake <jaker@Jakes-MacBook.local>
2014-05-25 14:01:57 -0500

    fixed footer links center

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd5b8fd00b8097b76160a531f32bc2a44');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c2c1b180a667c0681198763caffa2a20');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/footer.php
+++ templates/footer.php
@@ -2,16 +2,16 @@
   <div class="container-fluid footer-container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   	<div class="row">
-  		<div class="footer-link-container col-md-9">
-		    <div class="footer-links col-md-3">
+  		<div class="footer-link-container col-md-6 clearfix">
+		    <div class="footer-links col-md-4">
 		    <h5> Boxc </h5>
 		    <?php wp_nav_menu( array( 'theme_location' => 'footer_navigation_boxc' ) ); ?>
 			</div>
-			<div class="footer-links col-md-3">
+			<div class="footer-links col-md-4">
 		    <h5> Social </h5>
 		    <?php wp_nav_menu( array( 'theme_location' => 'footer_navigation_social' ) ); ?>
 			</div>
-		    <div class="footer-links col-md-3">
+		    <div class="footer-links col-md-4">
 		    <h5> Contact Us </h5>
 		    <?php wp_nav_menu( array( 'theme_location' => 'footer_navigation_contact' ) ); ?>
 			</div>
