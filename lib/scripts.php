ee3f8043665d5ecf5f34cea05bd02e751e6454ea
Jake <jaker@Jakes-MacBook.local>
2014-05-25 18:11:56 -0500

    remove form to custom

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e0d441357e7190956276b3fdd1f3045b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd6177e9fe356dab4fd134a391999630b');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
