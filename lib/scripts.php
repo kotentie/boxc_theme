08f663b5cbc8d09a53b336a68a4732de36c6dbd3
Jake <jaker@Jakes-MacBook.local>
2014-05-25 18:26:12 -0500

    add nav decouple

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1dc5bcd84274d5941a003e172ba1cccc');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c641a3ec359af8f0f494a988b5bb1122');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
