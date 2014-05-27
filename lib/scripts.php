b5e1666d60e9c4fcf453207236462c363f7fac5a
Jake <jaker@Jakes-MacBook.local>
2014-05-27 17:24:31 -0500

    remove breakpoints form

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'be2d08534c91f3bb1feed80003ae76b4');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '25caa6c82590b943224fc981ecb01125');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
