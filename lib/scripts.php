714db87eda1d2d3ecf6bee9f7a1c47e99e8e841c
Jake <jaker@Jakes-MacBook.local>
2014-06-12 12:34:58 -0500

    fix front css to no longer rely on padding

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e05e62a09d2f1c936c08dbd910b5e1aa');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '399bbc635a46b86bc5b092c90f07c834');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
