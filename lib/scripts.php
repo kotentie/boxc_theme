f5fcc4e9af7216d96fc06febb579762a52fa0e27
Jake <jaker@Jakes-MacBook.local>
2014-06-16 16:43:21 -0500

    add less vars

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5530ef89ab636848daf8e7f684ef4b56');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c1322475301ab1ecb90dafcd7b5b7f37');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
