2c9ae91161f2eab8738f8de61e894a63fe4930aa
Jake <jaker@Jakes-MacBook.local>
2014-06-24 13:21:27 -0500

    remove margin footer add to wrap

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6e83c8630ce2b7c5bcc1202254978655');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '173a86b7c1fa32116e7991a9541a4d4c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
